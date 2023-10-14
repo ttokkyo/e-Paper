284b7c935121e4eea31428171b5eb5c8478addc3
ttokkyo <vien.truong@gmail.com>
2023-10-13 19:28:39 -0700

    Hard code for Raspberry Pi

--- RaspberryPi_JetsonNano/python/lib/waveshare_epd/epdconfig.py
+++ RaspberryPi_JetsonNano/python/lib/waveshare_epd/epdconfig.py
@@ -230,12 +230,12 @@ class SunriseX3:
         self.GPIO.cleanup([self.RST_PIN, self.DC_PIN, self.CS_PIN, self.BUSY_PIN], self.PWR_PIN)
 
 
-if os.path.exists('/sys/bus/platform/drivers/gpiomem-bcm2835'):
-    implementation = RaspberryPi()
-elif os.path.exists('/sys/bus/platform/drivers/gpio-x3'):
-    implementation = SunriseX3()
-else:
-    implementation = JetsonNano()
+#if os.path.exists('/sys/bus/platform/drivers/gpiomem-bcm2835'):
+implementation = RaspberryPi()
+#elif os.path.exists('/sys/bus/platform/drivers/gpio-x3'):
+#    implementation = SunriseX3()
+#else:
+#    implementation = JetsonNano()
 
 for func in [x for x in dir(implementation) if not x.startswith('_')]:
     setattr(sys.modules[__name__], func, getattr(implementation, func))
